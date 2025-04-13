9aaf414ba0e32e7225905e847f29dc93fa46d949
ushivam75 <ushivam75@gmail.com>
2025-04-13 17:55:06 +0530

    App Desiging

---
+++ src/components/CartDetails.js
@@ -0,0 +1,124 @@
+import React from "react";
+import "../styles/cartstyle.css";
+
+const CartDetails = () => {
+  let arr = [0, 1];
+  return (
+    <>
+      <div className="row justify-content-center m-0">
+        <div className="col-md-8 mt-5 mb-5 cardsdetails">
+          <div className="card">
+            <div className="card-header bg-dark p-3">
+              <div className="card-header-flex">
+                <h5 className="text-white m-0">Cart Calculation(1)</h5>
+                {arr.length > 0 ? (
+                  <button className="btn btn-danger mt-0 btn-sm">
+                    <i className="fa fa-trash-alt me-2"></i>
+                    <span>Empty Cart</span>
+                  </button>
+                ) : (
+                  ""
+                )}
+              </div>
+            </div>
+            <div className="card-body p-0">
+              {arr.length === 0 ? (
+                <table className="table cart-table mb-0">
+                  <tbody>
+                    <tr>
+                      <td colSpan={6}>
+                        <div className="cart-empty">
+                          <i className="fa fa-shopping-cart">
+                            <p>Your cart is empty</p>
+                          </i>
+                        </div>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              ) : (
+                <table className="table cart-table mb-0 table-responsive-sm">
+                  <thead>
+                    <tr>
+                      <th>Action</th>
+                      <th>Product</th>
+                      <th>Name</th>
+                      <th>Price</th>
+                      <th>Qty</th>
+                      <th className="text-right">
+                        <span id="amount" className="amount">
+                          Total Amt
+                        </span>
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {arr.map((data, index) => {
+                      return (
+                        <>
+                          <tr>
+                            <td>
+                              <button className="prdct-delete">
+                                <i className="fa fa-trash-alt mr-2"></i>
+                              </button>
+                            </td>
+                            <td>
+                              <div className="product-img">
+                                <img src="/logo192.png" alt="" />
+                              </div>
+                            </td>
+                            <td>
+                              <div className="product-name">
+                                <p>punjabi</p>
+                              </div>
+                            </td>
+                            <td>300</td>
+                            <td>
+                              <div className="prdct-qty-container">
+                                <button className="prdct-qty-btn" type="button">
+                                  <i className="fa fa-minus"></i>
+                                </button>
+                                <input
+                                  text="text"
+                                  className="qty-input-box"
+                                  value={1}
+                                  disabled
+                                  name=""
+                                  id=""
+                                />
+                                <button className="prdct-qty-btn" type="button">
+                                  <i className="fa fa-plus"></i>
+                                </button>
+                              </div>
+                            </td>
+                            <td className="text-right">400</td>
+                          </tr>
+                        </>
+                      );
+                    })}
+                  </tbody>
+                  <tfoot>
+                    <tr>
+                      <th>&nbsp;</th>
+                      <th colSpan={3}>&nbsp;</th>
+                      <th>
+                        Item in cart<span className="ml-2 mr-2">:</span>
+                        <span className="text-danger">4</span>
+                      </th>
+                      <th className="text-right">
+                        Total price<span className="ml-2 mr-2">:</span>
+                        <span className="text-danger">4</span>
+                      </th>
+                    </tr>
+                  </tfoot>
+                </table>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CartDetails;
---
+++ src/components/Home.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import "../styles/style.css";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import CardsData from "./CardData";
+
+const Home = () => {
+  const [cardData, setCardData] = useState(CardsData);
+  return (
+    <>
+      <section className="inteam_section mt-4 container">
+        <h2 className="px-4" style={{ fontWeight: "400" }}>
+          Resturants Open in Delhi Now
+        </h2>
+        <div className="row mt-2 d-flex justify-content-around align-itmes-center">
+          {cardData.map((card, index) => {
+            return (
+              <>
+                <Card
+                  style={{ width: "22rem", border: "none" }}
+                  className="hove mb-4"
+                >
+                  <Card.Img variant="top" className="cd" src={card.imgdata} />
+
+                  <div className="card_body">
+                    <div className="upper_data d-flex justify-content-between align-items-center">
+                      <h4 className="mt-2">{card.dish}</h4>
+                      <span>{card.rating}&nbsp;⭐</span>
+                    </div>
+                    <div className="lower_data d-flex justify-content-between">
+                      <h5>{card.address}</h5>
+                      <span>{card.price}</span>
+                    </div>
+                    <div className="extra"></div>
+                    <div className="last_data d-flex justify-content-between align-items-center">
+                      <img className="limg" src={card.arrimg} alt="" />
+                      <Button
+                        style={{
+                          width: "150px",
+                          background: "#ff3054db",
+                          border: "none",
+                        }}
+                        variant="outline-light"
+                        className="mt-2 mb-2"
+                      >
+                        Add to cart
+                      </Button>
+                      <img className="laimg" src={card.delimg} alt="" />
+                    </div>
+                  </div>
+                </Card>
+              </>
+            );
+          })}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Home;
